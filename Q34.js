825c4382074df547029f82715355cac90b0ba7a7
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:51:51 -0800

    Create Q34.js

---
+++ Q34.js
@@ -0,0 +1,5 @@
+let pizza=["fajita", "BBQ", "peproni", "Cheez"]
+for(i=0; i<pizza.length; i++){
+  console.log("I love "+pizza[i]+" pizza.");
+}
+console.log("how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!");
