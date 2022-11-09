34ae23d1c79497615fc31baa6a2fb37c3f2fade1
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:47:43 -0800

    Create Q29.js

---
+++ Q29.js
@@ -0,0 +1,17 @@
+let faorite_fruits= ["guava", "mango", "apple", "peach", "pineapple" ]
+ 
+if (favorite_fruits.includes("guava")) {
+    console.log("guava is a teasty food.") 
+}
+if (favorite_fruits.includes("mango")) {
+    console.log("mango is a teasty food.") 
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("apple is a teasty food.") 
+}
+if (favorite_fruits.includes("peach")) {
+    console.log("peach is a teasty food.") 
+}
+if (favorite_fruits.includes("pineapple")) {
+    console.log("pineapple is a teasty food.") 
+}
