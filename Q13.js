9f5eb9c4937060b335feb7ee335a92ad2a1d91b2
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:32:01 -0800

    Create Q13.js

---
+++ Q13.js
@@ -0,0 +1,4 @@
+let arr= ['Kawasaki H2R','BMW I8','Tesla model S']
+  console.log(arr[0] + ' is the fastest bike in world.')
+  console.log(arr[1] +' is an expensive car.')
+  console.log(arr[2] +' is my deam car.')
