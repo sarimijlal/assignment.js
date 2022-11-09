6252392b2e8533749b25d4472db0e2320233e03d
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:43:17 -0800

    Create Q26.js

---
+++ Q26.js
@@ -0,0 +1,14 @@
+let alien_color = "green"
+if(alien_color == "green"){
+    console.log("The player just earned 5 points for shooting down the alien.")
+}
+else if(alien_color != "green"){
+  console.log("The player just earned 10 points.")
+}
+//version 2:
+if(alien_color == "red"){
+    console.log("The player just earned 5 points for shooting down the alien.")
+}
+else if(alien_color == "green"){
+  console.log("The player just earned 10 points.")
+}
