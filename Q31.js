8e67997498ab3165da84c2e339f31b8f44b094e4
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:50:26 -0800

    Create Q31.js

---
+++ Q31.js
@@ -0,0 +1,10 @@
+let arr = []
+ if(arr.length == 0){
+    console.log("We need users")
+}
+arr =["chris", "urs", "admin", "phil", "andrie"]
+ 
+    for( let i=arr.length;i>0;i--){
+    arr.pop()
+}
+console.log(arr)
