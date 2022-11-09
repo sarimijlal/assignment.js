1f5d7b163a402115b83e9e90811c0fda33f71cb3
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:45:58 -0800

    Create Q27.js

---
+++ Q27.js
@@ -0,0 +1,35 @@
+let alien_color = "green"
+if(alien_color == "green"){
+    console.log("The player earned 5 points.")
+}
+else if(alien_color == "yellow"){
+  console.log("The player earned 10 points.")
+}
+
+else if (alien_color == "red"){
+    console.log("The player earned 15 points.")
+}
+//version 02:
+let alien_color1 = "yellow"
+if(alien_color1 == "green"){
+    console.log("The player earned 5 points .")
+}
+else if(alien_color1 == "yellow"){
+  console.log("The player earned 10 points.")
+}
+
+else if(alien_color1 == "red"){
+    console.log("The player earned 15 points.")
+}
+//version 03: 
+let alien_color2 = "red"
+if(alien_color2 == "green"){
+    console.log("The player earned 5 points.")
+}
+else if(alien_color2 == "yellow"){
+  console.log("The player earned 10 points.")
+}
+
+else if(alien_color2 == "red"){
+    console.log("The player earned 15 points.")
+}
