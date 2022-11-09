996eeede20e1d41ca9044b8d2dd2f2ad3cf49bf2
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:52:38 -0800

    Create Q35.js

---
+++ Q35.js
@@ -0,0 +1,14 @@
+let animal=["cow", "kangaroo", "cat"]
+for(i=0; i<animal.length; i++){
+    if(animal[i]=="kangaroo"){
+        console.log("Babies of kangaroos are very tiny");
+    }
+    else if (animal[i]=="cow") {
+        console.log("cow gives us milk");
+    }
+    else if (animal[i]=="cat") {
+        console.log("cat is a lovely pet");
+    }
+
+}
+console.log("All these animals are mamals.");
