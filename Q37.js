0265ee6b478146ae59fb91310ea7d27d497e531b
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:54:08 -0800

    Create Q37.js

---
+++ Q37.js
@@ -0,0 +1,8 @@
+let make_shirt=(size ="Large", message = "I Love Java Script")=>{
+  if(size == "Large" || size == "Medium"){
+    console.log(`The size of the shirt is ${size} and ${message}`)
+  }
+  else{
+      console.log(`The size of the shirt is ${size} and ${message}`)
+  }
+}
