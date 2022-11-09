e811ef6423cbe3315f05e38d0bb59d3bb57a6347
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:55:10 -0800

    Create Q39.js

---
+++ Q39.js
@@ -0,0 +1,6 @@
+const city_country=(city , country)=>{
+  console.log(`${city},${country}`)
+}
+city_country("Islamabad","Pakistan")
+city_country("Pindi","Pakistan")
+city_country("Istanbul","Turkey")
