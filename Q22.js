ee5dfa04a7d4c4031894a3007d0be265f2924821
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:38:44 -0800

    Create Q22.js

---
+++ Q22.js
@@ -0,0 +1,25 @@
+let 
+a=5
+b=7
+//1st statement
+console.log("a==b? i predict false.")
+console.log(a==b)
+console.log('\"b" has a grater value than \"a".')
+//second statement
+console.log('\n\"a!=a? i predict true."')
+console.log(b>a)
+//third statement
+console.log("\nb>a? i predict true.")
+console.log(b>a)
+//fourth statement
+console.log("\na>b? i predict false")
+console.log(a>b)
+//fifth statement
+let 
+car="Mustang"
+fav_car="Supra"
+console.log("\nfavourite car==Mustang? i predict false.")
+console.log(fav_car=="Mustang")
+//sixth statement
+console.log("\nfavourite car==Supra? i predict true.")
+console.log(fav_car=="Supra")
