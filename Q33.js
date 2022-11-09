33e0eb44d950b631073234e70a79a80e36eebd4c
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:51:20 -0800

    Create Q33.js

---
+++ Q33.js
@@ -0,0 +1,15 @@
+let numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i]===1) {
+        console.log("1st")
+    }
+    else if (numbers[i]===2) {
+        console.log("2nd");
+    }
+    else if (numbers[i]===3) {
+        console.log("3rd");
+    }
+    else {
+        console.log(numbers[i]+"th");
+    }
+}
