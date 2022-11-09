3c0a279907e34b591dbb418dce38e5fc1cd64737
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:20:56 -0800

    Create q5.js

---
+++ q5.js
@@ -0,0 +1,3 @@
+var famous_person= ("Elon Musk");
+  var message= ("\"Some people don't like change, but you need to embrace change if the alternative is disaster.\"")
+  console.log(famous_person +" once said ," + message)
