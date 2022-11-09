488d067c70ef4a1ded68b4dc3f1ad4aa4017422b
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:54:38 -0800

    Create 38.js

---
+++ 38.js
@@ -0,0 +1,6 @@
+const describe_city=(city , country="Pakistan")=>{
+  console.log(`${city} is in ${country}`)
+}
+describe_city("Islamabad")
+describe_city("Pindi")
+describe_city("Istanbul","Turkey")
