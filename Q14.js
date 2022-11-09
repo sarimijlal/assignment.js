8c7f698aa3de059a61d96a1bdeec1401eb0bed3e
sarimijlal <115392957+sarimijlal@users.noreply.github.com>
2022-11-08 23:32:28 -0800

    Create Q14.js

---
+++ Q14.js
@@ -0,0 +1,4 @@
+  let friends= ['ahmed', 'zayan', 'shoaib'];
+  for (let i = 0; i <friends.length; i++){
+    console.log("Hi"+friends[i]+", i would love to have you on dinner at my place tonight.")
+}
